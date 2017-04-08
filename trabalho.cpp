e399e5bcf11ea99d7eb2388b749a3f28747ce585
gabriel <gabriel_alves_ds@hotmail.com>
2017-04-08 17:59:58 -0300

    Nova versao

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,117 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 typedef struct nodo
 {
-
  float dado;
  int lin, col;
  struct nodo *prox;
 }Matriz_Esparsa;
 
 
-Matriz_Esparsa* Criar_Nodo() //1 alocaçao de memoria
+void inicializar_matriz(Matriz_Esparsa **N)
 {
-  Matriz_Esparsa *v;
-   v = (Matriz_Esparsa*) malloc(sizeof(Matriz_Esparsa));
-
-   	if(!v){
-		printf("\nErro na alocacao");
-			exit(0);
-  }
-  return v;
+ 	*N = NULL;
+}
+Matriz_Esparsa* Cria_Nodo()
+{
+	Matriz_Esparsa *prox;
+		prox = (Matriz_Esparsa*) malloc(sizeof(Matriz_Esparsa));
+			if(!prox)
+			 {
+				printf("\nErro de alocacao");
+					exit(0);
+			}			
+		return prox;
 }
 
-void nodo_col_lin (Matriz_Esparsa *p)
-{
 
-}
-
-void insere(Matriz_Esparsa) // 2
-{
-
-}
-void busca(Matriz_Esparsa *v , int *prox) //3busca
-{
-	//	Matriz_Esparsa *p;
-	//	for(p=v; p!=NULL; p=p->prox){
-	//		if(p->infor ==v)
-	//		 return p;
-	//}
-	//return NULL;
-}
-void libera_memoria(nodo* v) //4liberar memoria
-{
-	nodo *p = v;
-		while(p != NULL){
-		nodo *x = p->prox;
-		free(p);
-		p = x;
+float busca_lista(Matriz_Esparsa **N, int li, int co)
+	{
+		Matriz_Esparsa *axs;
+			if(*N!=NULL)
+			{
+			while((axs->lin != li)&&(axs->col != co)||(axs->prox!=NULL))
+			{
+			axs=axs -> prox;
+			}
+				if((axs->lin != li)&&(axs->col !=co))
+				{
+				return axs->dado;
+				}
+				else
+				{
+				return 0;
+				}
+			{
+			printf("\n Matriz vazia!!!");
+			}
 		}
 }
-void inseri_lista(Matriz_Esparsa *v)
-{
-    int lin;
-	printf("\nInseri numero de linhas e colunas");
-	  //while(lin) ou um for{
-	printf("\nInseri numero de linhas : \n");
-	scanf("%d", lin);
- }
 
 
-void soma() //arruma
+
+void libera_memoria(Matriz_Esparsa **N)
+	{
+		Matriz_Esparsa *p = *N, *axs;
+			while(p != NULL)
+				{
+				axs = p->prox;
+				free(axs);
+				}
+				inicializar_matriz(N);
+	}
+	
+void insere(Matriz_Esparsa **N, float dado , int li, int co)
+	{
+	
+	Matriz_Esparsa *nv, *axs;
+		
+		nv = Cria_Nodo();
+		nv-> dado=dado;
+		nv->lin = li;
+		nv->col = co;
+		nv->prox = NULL;
+		if(*N == NULL)
+		{
+		*N = nv;
+		}
+		else
+		{
+		axs = *N;
+		while(axs->prox !=NULL)
+		{
+		axs = axs -> prox;
+		}
+		axs->prox = nv;
+		}
+	}	
+	
+Matriz_Esparsa le_dado()
 {
-  //  int i;
-      /* pra cada elemento da matriz */
-   // for (i = 0; i < lin * col[] ; i++) {
-          /* soma valor se elemento Ã© nao-nulo */
-       // if (* (s.sp + i) != 0)  * (s.sp + i) = (* (s.sp + i)+ constante);///
-//}
+	Matriz_Esparsa *Nodo;
+	int li , co , g ,h;
+		float s;
+		
+		inicializar_matriz(&Nodo);
+		printf("\nDigite quantidade de linha");
+		scanf("%d", &li);
+		printf("\nDigite quantidade de coluna");
+		scanf("%d" , &co);
+			for(g=0;g<li-1;g++)
+			{
+			for(h=0;h<co-1;h++)
+			{
+			system("cls");
+				printf("\nDigite elementos para inserção");
+					scanf("%f", &s);
+					if(s !=0)
+					{
+						insere(&Nodo, s, li, co);
+					}
+				}
+			}
+		return *Nodo;
 }
 
-void subtrai()
-{
-}
-
-void multiplica()
-{
-}
-void matriz_tranposta()
-{
-}
-void imprime_matriz()
-{
-}
-void imprime_diagonais()
-{
-}
-
-main()//
+int main()
 {
 	int lin,col;
 	int opc;
 	printf("\n Escolha uma opcao:");
 		printf("\n 1-Iniciar");
 		printf("\n 2-Sair");
-		printf("\n 3-Opcao : ");
 		printf("Opcao :");scanf("%d", &opc);
-			return opc;
-
-				switch(opc)
+			
+			switch(opc)
 	{
 	case 1:
-	printf("\nDigite numero de linha :");
-	scanf("%d", &lin);
-	printf("\nDigite numero de coluna :");
-	scanf("%d", &col);
+		printf("\nDigite numero de linha :");
+		scanf("%d", &lin);
+		printf("\nDigite numero de coluna :");
+		scanf("%d", &col);
 	break;
-
+	
 	case 2:
-
-	default:
-
-		printf("\nComando invalido\n\n");
+		default:
+		printf("\nSaindo...\n\n");
+	break;
 	}
-	return lin;
-	return col;
+
 }
+	
